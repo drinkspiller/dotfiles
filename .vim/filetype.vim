3d4b781818c9293456e05af0529aff3829fd9824
Skye Giordano <github@syntax101.com>
2013-03-21 14:26:27 +0000

    adding nginx syntax highlighting per http://is.gd/6XG3wJ and http://is.gd/hkim9x

---
+++ .vim/filetype.vim
@@ -0,0 +1,2 @@
+au BufRead,BufNewFile /usr/local/nginx/conf/* set ft=nginx
+au BufRead,BufNewFile /usr/local/nginx/vhosts/* set ft=nginx
--- difftool-kdiff3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Passed the following parameters from git
-#  --auto --L1 "somefile (A)" --L2 "somefile (B)" /tmp/4VhUw0_somefile somefile
-
-LOCAL=$6
-REMOTE=$7
-
-"C:/Program Files/KDiff3/kdiff3.exe" $(cygpath -w "$LOCAL") "$REMOTE"
