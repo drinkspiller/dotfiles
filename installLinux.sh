ab88ac881aaa34a53d6fbac8e8af914005eee845
Skye Giordano <skyebot@google.com>
2020-04-16 15:30:16 -0600

    Corrects live-server package name to incolude hypen.

--- installLinux.sh
+++ installLinux.sh
@@ -65,6 +65,6 @@ $run_as bash <<EOF
   #npm install -g carbon-now-cli
   npm install -g chokidar-cli
   npm install -g fkill-cli
-  npm install -g liveserver
+  npm install -g live-server
   #npm install -g ytdl
 EOF
